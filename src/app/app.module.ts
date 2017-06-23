876d36134f8f0cf50e81b3c54d102956afe59570
gooner573 <lkb573@gmail.com>
2017-06-23 13:43:24 +0900

    Angular Start_20170623 Hero Tutorial_chap.5,6 ..ing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HeroDetailComponent} from "./hero-deatil.component";
+import {HeroesComponent} from "./heroes.component";
+import {HeroService} from "app/hero.service";
+import {RouterModule} from "@angular/router";
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
